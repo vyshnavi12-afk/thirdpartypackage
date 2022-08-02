0d3003eaac082e70133ce85368fc576d4690a96d
vyshnavi12-akf <gvyshnavi2003@gmail.com>
2022-08-02 11:45:41 +0000

    solution

---
+++ index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactPlayer from "react-player";
+<ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />;
+document.render(<ReactPlayer />, document.getElementById("root"));
